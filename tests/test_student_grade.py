78380c2aea2b8808857bbb790250d28be2ce4791
anupborker <anupborker@gmail.com>
2026-02-04 10:52:42 +0530

    6th commit

--- tests/test_student_grade.py
+++ tests/test_student_grade.py
@@ -1,21 +1,21 @@
-from src.student_grade_calculator import get_grade
+from src.student_grade_calculator import calculate_grade
 
 def test_A_plus():
-    _, grade = get_grade([95, 92, 90, 93, 94])
+    _, grade = calculate_grade([95, 92, 90, 93, 94])
     assert grade == "A+"
 
 def test_A():
-    _, grade = get_grade([80, 78, 76, 75, 77])
+    _, grade = calculate_grade([80, 78, 76, 75, 77])
     assert grade == "A"
 
 def test_B():
-    _, grade = get_grade([65, 62, 60, 63, 61])
+    _, grade = calculate_grade([65, 62, 60, 63, 61])
     assert grade == "B"
 
 def test_C():
-    _, grade = get_grade([52, 55, 50, 53, 54])
+    _, grade = calculate_grade([52, 55, 50, 53, 54])
     assert grade == "C"
 
 def test_Fail():
-    _, grade = get_grade([40, 45, 48, 42, 44])
+    _, grade = calculate_grade([40, 45, 48, 42, 44])
     assert grade == "Fail"
